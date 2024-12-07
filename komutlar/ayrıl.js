c560b2fd168b02eed29b93c5fd067996bd097b2e
onurr_k <145716470+onurrrk@users.noreply.github.com>
2024-12-07 11:51:36 +0300

    Add files via upload

---
+++ komutlar/ayrıl.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: "ayrıl",
+  code: `
+$deleteCommand
+$botLeave
+$onlyForIDs[67;**Bu Komutu Sadece Botun Yapımcısı Kullanabilir!**]
+`
+} 
--- komutlar/consol.js
+++ komutlar/consol.js
@@ -1,1 +1,7 @@
-
+module.exports = {
+  name: "consol",
+  code: `
+  $exec[$message]
+  $onlyForIDs[629700365725466634;**Bu Komutu Sadece Botun Yapımcısı Kullanabilir!**]
+  `
+}
